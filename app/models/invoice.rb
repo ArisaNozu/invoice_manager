2e3d72fcd2428b1cf4411b85524f37a06abbb797
ArisaNozu <myshkin88@gmail.com>
2025-08-07 14:49:28 +0900

    invoiceモデルにアソシエーション追加

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,10 +1,22 @@
 class Invoice < ApplicationRecord
-
+  # アソシエーション
 belongs_to :user
 belongs_to :client
 acts_as_taggable_on :tags
 
 
+  # バリデーション
+  validates :status, presence: true
+  validates :receipt_method, presence: true
+  validates :client_id, presence: true
+  validates :due_date, presence: true
+  validates :received_date, presence: true
+  validates :is_qualified_invoice_issuer, presence: true
+  validates :net_amount, presence: true
+  validates :tax_amount, presence: true
+  validates :total_amount, presence: true
+  validates :tax_rate, presence: true
 
 
+  
 end

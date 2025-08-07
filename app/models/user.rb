857fc38c4e2d81a7f5913fa84a0003a9261e5fe5
ArisaNozu <myshkin88@gmail.com>
2025-08-07 15:47:03 +0900

    userモデルにバリテーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # ActiveHash（department）との関連
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :department
+
+  # バリデーション
+  validates :full_name, presence: true
+  validates :employee_number, presence: true, uniqueness: true
+  validates :department_id, presence: true, numericality: { other_than: 0, message: "を選択してください" }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :encrypted_password, presence: true
+
+
 end

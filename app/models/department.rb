c9e8885be0e564a7a6d0cf03fcff113c8f0817f2
ArisaNozu <myshkin88@gmail.com>
2025-08-09 20:46:53 +0900

    departmentモデル作成

---
+++ app/models/department.rb
@@ -0,0 +1,10 @@
+class Department < ActiveHash::Base
+ self.data = [
+   { id: 1, name: '---' },
+   { id: 2, name: '営業部' },
+   { id: 3, name: '開発部' },
+   { id: 4, name: '総務部' },
+   { id: 5, name: '人事部' },
+   { id: 6, name: '経理部' }
+ ]
+end

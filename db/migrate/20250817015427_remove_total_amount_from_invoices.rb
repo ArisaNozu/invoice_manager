9d79a26276381b0276787ae4905d823eb01581db
ArisaNozu <myshkin88@gmail.com>
2025-08-17 11:13:21 +0900

    マイグレーションを「存在すれば削除」に書き換え→実行

--- db/migrate/20250817015427_remove_total_amount_from_invoices.rb
+++ db/migrate/20250817015427_remove_total_amount_from_invoices.rb
@@ -1,5 +1,10 @@
-class RemoveTotalAmountFromInvoices < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :invoices, :total_amount, :integer
+class RemoveTotalAmountFromInvoices < ActiveRecord::Migration[6.1]
+  def up
+    remove_column :invoices, :total_amount if column_exists?(:invoices, :total_amount)
+  end
+
+  def down
+    # 復元用（必要なら）
+    add_column :invoices, :total_amount, :integer unless column_exists?(:invoices, :total_amount)
   end
 end

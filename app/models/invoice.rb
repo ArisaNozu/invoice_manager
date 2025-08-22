1c97a122a42e79caf344804e0212f8d67c3acca1
ArisaNozu <myshkin88@gmail.com>
2025-08-22 15:07:24 +0900

    一覧のビュー調整

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,8 +1,9 @@
 class Invoice < ApplicationRecord
   # アソシエーション
-belongs_to :user
-belongs_to :client, foreign_key: :client_code, primary_key: :client_code, optional: true
-acts_as_taggable_on :tags
+  belongs_to :user
+  belongs_to :client, foreign_key: :client_code, primary_key: :client_code, optional: true
+  acts_as_taggable_on :tags
+
 
   # バリデーション
   validates :status, presence: true
@@ -19,6 +20,14 @@ acts_as_taggable_on :tags
   validates :tax_amount, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   validates :subject, length: { maximum: 100 }
+
+
+  # ActiveHashの設定
+  extend ActiveHash::Associations::ActiveRecordExtensions
+belongs_to_active_hash :status_master,            class_name: "Status",            foreign_key: :status
+belongs_to_active_hash :receipt_method_master,    class_name: "ReceiptMethod",     foreign_key: :receipt_method
+belongs_to_active_hash :tax_rate_master,          class_name: "TaxRate",           foreign_key: :tax_rate
+belongs_to_active_hash :receipt_frequency_master, class_name: "ReceiptFrequency",  foreign_key: :receipt_frequency
 
   # 合計金額＝本体価格＋消費税額
   def total_amount
